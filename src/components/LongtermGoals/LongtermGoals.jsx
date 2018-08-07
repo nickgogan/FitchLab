3834ea108245f632989d441c7397555b4c0e5c70
Nick GOGAN <GOGAN@sinequa.com>
2018-08-07 15:12:12 -0400

    Heroku deployment failed, trying again.

--- src/components/LongtermGoals/LongtermGoals.jsx
+++ src/components/LongtermGoals/LongtermGoals.jsx
@@ -5,7 +5,7 @@ import styles from './LongtermGoals.postcss';
 const LongtermGoals = () => (
   <div>
     <div className={styles.container}>
-      <h1 className={styles.heading}>Longterm Goals</h1>
+      <h1 className={styles.heading}>Longterm Goals!</h1>
       <br />
       <h3 className={styles.text_title}>How genes control form.</h3>
       <br />
